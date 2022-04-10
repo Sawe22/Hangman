2733c8256097c693d9fd5e808e8053f4e2895386
Sawe22 <102390580+Sawe22@users.noreply.github.com>
2022-04-09 23:54:42 -0700

    Create wordlist.py

---
+++ wordlist.py
@@ -0,0 +1,42 @@
+list_Of_Words = ["Rai", "Kwantlen", "Zimmerman", "Joe", "laptop", "edible", "fromage", "cactus", "pogchamp", "chris", "raj", "jeffey", "stuco", "class", "bomb", "syria", "russia", "politics", "dyslexia", "disability",]
+
+
+HANGMAN_PICS = ['''
++---+
+    |
+    |
+    |
+    ===''', '''
++---+
+O   |
+    |
+    |
+    ===''', '''
++---+
+O   |
+|   |
+    |
+    ===''', '''
++---+
+O   |
+/|  |
+    |
+    ===''', '''
++---+
+O   |
+/|\ |
+    |
+    ===''', '''
++---+
+O   |
+/|\ |
+/   |
+    ===''', '''
++---+
+O   |
+/|\ |
+/ \ |
+    ===''']
+
+list_of_congrats = ["Good job", "Not bad", "I'm impressed", "Let's go!", "Keep going!", "Almost there!", "You're going great!",]
+list_of_rejects = ["Are u dum?", "Might as well quit now", "Start using your brain", "I hope your battery dies", "This is why we cant have nice things", "Did you even try?", "Why don't you ever think?"]
